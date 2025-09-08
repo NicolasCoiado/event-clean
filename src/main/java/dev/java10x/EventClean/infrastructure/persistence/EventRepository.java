7e8dc4f49b26946939964743b8bd7e9db0a63fe4
NicolasCoiado <nicolasmcoiado@gmail.com>
2025-09-08 14:57:13 -0300

    fix: Adds the @Repository annotation

--- src/main/java/dev/java10x/EventClean/infrastructure/persistence/EventRepository.java
+++ src/main/java/dev/java10x/EventClean/infrastructure/persistence/EventRepository.java
@@ -1,9 +1,11 @@
 package dev.java10x.EventClean.infrastructure.persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface EventRepository extends JpaRepository<EventEntity, Long> {
     public Optional<EventEntity> findEventByIdentifier (String identifier);
 }
